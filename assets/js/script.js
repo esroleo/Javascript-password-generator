ca845308699e40ccca984a68561479c068109083
Esteban Romero <esroleo@gmail.com>
2020-12-17 22:11:11 -0500

    inital testing completed

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  window.alert(" About to generate password");
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+  
+  //passwordText.value = password;
+  document.getElementById("password").readOnly = false;
+  document.getElementById("password").value = "Fifth Avenue, New York City";
+  document.getElementById("password").readOnly = true;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
