3c49147316c2d9e592552d9255de3cd7267d10e0
Esteban Romero <esroleo@gmail.com>
2020-12-18 15:04:29 -0500

    lenght has been validated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Assignment code here
 
+var arraySplit = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -9,13 +10,13 @@ var generatePassword = function() {
   window.alert("The criteria for password generator are lowercase, uppercase, numeric, and/or special characters");
   var userInput = window.prompt("Select criteria for password (you can select more than one) (use space): 1=lowercase 2=uppercase 3=numeric 4=special characters ").split(" ");
   //Split the user input
-  var arraySplit = userInput.slice(0,userInput.length);
+  arraySplit = userInput.slice(0,userInput.length);
   // Tell the user what they have selected.
   window.alert("You have selected " + arraySplit);
   console.log(userInput);
   //console.log(userInput[0]);
 
-
+  
   for (var i = 0; i < userInput.length; i++) {
     // Tell the user what they have selected
     var inputCriteria = userInput[i];
@@ -23,25 +24,42 @@ var generatePassword = function() {
     //Save output in variable at the end of the loop to generate a password of randomness.
     switch(inputCriteriaInteger) {
       case 1:
-        console.log("We are in the loop and case 1 achieve");
       case 2:
-        console.log("We are in the loop and case 2 achieve");
       case 3:
-        console.log("We are in the loop and case 3 achieve");
       case 4:
-        console.log("We are in the loop and case 4 achieve");
+        console.log("We are in the loop and case good numbers passed achieve");
+        break;
       default:
-        if (!inputCriteriaInteger)
+        //if (!inputCriteriaInteger)
         console.log(" Please select a criteria using numbers. " + "User input: " + inputCriteria + " is invalid");
+        generatePassword();
     }
   }
+
+  // Call Function to validate length
+  passLengght();
+
+  function passLengght () {
+
+    //Ask user for lenght of password.
+    window.alert("How long do you want your password to be from 8-128 characters.\n E.g 20");
+    var pLenght = parseInt(window.prompt("Please enter lenght"));
+    if (pLenght < 8 || pLenght > 128) {
+      window.alert("Please enter a valid number between 8-128");
+      passLengght();
+
+    } else if (!pLenght) {
+      window.alert("Please enter a valid number between 8-128")
+      passLengght();
+    } else {
+      window.alert("You have entered a valid lenght of: " + pLenght + "\n Password will now be generated");
+    }
+  }
+
+
+
   
 
-  switch(userInput) {
-    case "1":
-      console.log(" I am in switch case 0 input" + userInput[0] );
-
-  }
 }
 
 // Write password to the #password input
@@ -52,7 +70,9 @@ function writePassword() {
   
   //passwordText.value = password;
   document.getElementById("password").readOnly = false;
-  document.getElementById("password").value = "Fifth Avenue, New York City";
+  //document.getElementById("password").value = "Fifth Avenue, New York City";
+  document.getElementById("password").value = arraySplit;
+  
   document.getElementById("password").readOnly = true;
 
 }
