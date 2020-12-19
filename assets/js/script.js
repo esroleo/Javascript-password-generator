d153365f920f9d846eb9468cee6016569a955560
Esteban Romero <esroleo@gmail.com>
2020-12-19 05:26:06 -0500

    validate input and navigation completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,43 @@
 // Assignment code here
 
 var arraySplit = "";
+var exitApplication = false;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Call function to genereat password
 var generatePassword = function() {
-  //Alert user for criteria
+  // Alert user for criteria
   window.alert("The criteria for password generator are lowercase, uppercase, numeric, and/or special characters");
-  var userInput = window.prompt("Select criteria for password (you can select more than one) (use space): 1=lowercase 2=uppercase 3=numeric 4=special characters ").split(" ");
-  //Split the user input
-  arraySplit = userInput.slice(0,userInput.length);
+  // Split into an array 
+  var userInput = window.prompt("Select criteria for password (you can select more than one) (use space): 1=lowercase 2=uppercase 3=numeric 4=special characters ");
+  // If user press cancel while trying to split it will fail, hence we need split after the fact.
+  // If statement required to split 
+  if (!userInput) {
+    exitApplication = window.alert("Restarting the application");
+    exitApplicationForm();
+  } else {
+    // Continue with the application
+    // User put a valid input to try to split to array
+    // We can now continue to slice to show the output to user
+    userInput = userInput.split(" ");
+    arraySplit = userInput.slice(0,userInput.length);
+  }
+  
   // Tell the user what they have selected.
   window.alert("You have selected " + arraySplit);
-  console.log(userInput);
+  //console.log(userInput);
   //console.log(userInput[0]);
 
-  
+  // Create a for loop to iterate the user input to determine if valid criteria input
   for (var i = 0; i < userInput.length; i++) {
-    // Tell the user what they have selected
+    // Take one item/index from the user input
     var inputCriteria = userInput[i];
+    // Convert this output into integer for further validation
     var inputCriteriaInteger = parseInt(inputCriteria);
-    //Save output in variable at the end of the loop to generate a password of randomness.
+    // Save output in variable at the end of the loop to generate a password of randomness.
+    // Switch case with fall through logic to arrive at conclusions
     switch(inputCriteriaInteger) {
       case 1:
       case 2:
@@ -30,9 +46,16 @@ var generatePassword = function() {
         console.log("We are in the loop and case good numbers passed achieve");
         break;
       default:
-        //if (!inputCriteriaInteger)
-        console.log(" Please select a criteria using numbers. " + "User input: " + inputCriteria + " is invalid");
-        generatePassword();
+        if (!inputCriteriaInteger) {
+          window.alert(" Do you wish to exit the application?");
+          generatePassword();  
+        }
+        // Iterate internall until valid input.
+        else { 
+          window.alert(" Please select a criteria using numbers. " + "User input: " + inputCriteria + " is invalid");
+          generatePassword();  
+        }
+        
     }
   }
 
@@ -41,7 +64,7 @@ var generatePassword = function() {
 
   function passLengght () {
 
-    //Ask user for lenght of password.
+    // Ask user for lenght of password.
     window.alert("How long do you want your password to be from 8-128 characters.\n E.g 20");
     var pLenght = parseInt(window.prompt("Please enter lenght"));
     if (pLenght < 8 || pLenght > 128) {
@@ -56,15 +79,11 @@ var generatePassword = function() {
     }
   }
 
-
-
-  
-
 }
 
 // Write password to the #password input
 function writePassword() {
-  window.alert(" About to generate password");
+  window.alert(" Lets check our password criteria options");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
@@ -72,14 +91,23 @@ function writePassword() {
   document.getElementById("password").readOnly = false;
   //document.getElementById("password").value = "Fifth Avenue, New York City";
   document.getElementById("password").value = arraySplit;
-  
   document.getElementById("password").readOnly = true;
 
 }
 
 
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Verify if we should start the application or not.
+function exitApplicationForm () {
+   if (exitApplication) {
+     // Exit the application by doing nothing.
+
+   } else {
+     // Start the application
+     writePassword();
+    }
+}
 
